856087712155d8964bc1b46f764c78d0183b3af9
Dullah Mtavya <mtavya420@gmail.com>
2023-01-28 14:51:24 +0900

    minor changes

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -17,7 +17,7 @@ const SignIn = () => {
   };
 
   useEffect(() => {
-    if (user && Object.keys(user).length !== 0) navigate("/");
+    if (user && Object.keys(user).length !== 0){ navigate("/")};
   }, [user]);
 
   return (
